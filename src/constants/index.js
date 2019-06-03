bbd3acd3ce3c9f8cc8ac16703c289005a982f2d2
Luciamo <lucianoeduardodantas@gmail.com>
2019-06-03 20:30:37 -0300

    Setting the constants for events and Weapon life time

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+export const ACTIONS = {
+  INIT_GAME:  'InitGame',
+  GAME_BREAK: 'GameBreak',
+  SHUTDOWN_GAME: 'ShutdownGame',
+  KILL: 'Kill',
+  CLIENT_CONNECT: 'ClientConnect',
+  CLIENT_USER_INFO_CHANGED: 'ClientUserInfoChanged',
+  ITEM: 'Item'
+}
+
+export const EXPIRE_TIME = 180;
